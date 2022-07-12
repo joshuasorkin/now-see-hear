900a38b563fff62d943786c80d4c892b789a60f1
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-07-12 14:11:41 -0700

    remove scripts from previous tutorial

--- public/javascripts/index copy.js
+++
@@ -1,9 +0,0 @@
-console.log("start index.js")
-document.getElementById('thisButton').addEventListener('click', function() {
-    console.log("you clicked a button at"+Date.now());
-    Mic.audioContext.resume().then(() => {
-        console.log('Playback resumed successfully');
-    });
-});
-console.log("end index.js")
-
